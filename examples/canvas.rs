31860e68ad79ad60d18d22ebdea7054f041eb7bb
Evan Almloff <evanalmloff@gmail.com>
2025-12-31 13:08:57 -0600

    add more examples

---
+++ examples/canvas.rs
@@ -0,0 +1,69 @@
+//! Modified from https://github.com/wasm-bindgen/wasm-bindgen/tree/main/examples/canvas to work with wry-bindgen
+
+use core::f64;
+
+use wasm_bindgen::prelude::*;
+use wry_testing::run;
+
+fn main() -> wry::Result<()> {
+    run(|| {
+        app();
+
+        // The type here doesn't matter, we just want to wait forever.
+        _ = wait_for_js_result::<i32>();
+    })
+}
+
+fn app() {
+    let window = web_sys::window().expect("should have a window in this context");
+    let document = window.document().expect("window should have a document");
+
+    // Below are some more advanced usages of the `Closure` type for closures
+    // that need to live beyond our function call.
+
+    // Add a clock with #current-time that updates every second.
+    let body = document.body().expect("document should have a body");
+    body.set_inner_html(
+        r#"<canvas id="canvas" height="150" width="150"></canvas>"#,
+    );
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
+
+}
---
+++ examples/paint.rs
@@ -0,0 +1,79 @@
+//! Modified from https://github.com/wasm-bindgen/wasm-bindgen/tree/main/examples/paint to work with wry-bindgen
+
+use core::f64;
+use wasm_bindgen::prelude::*;
+use wry_testing::run;
+use std::cell::Cell;
+use std::rc::Rc;
+
+fn main() -> wry::Result<()> {
+    run(|| {
+        app();
+
+        // The type here doesn't matter, we just want to wait forever.
+        _ = wait_for_js_result::<i32>();
+    })
+}
+
+fn app() {
+    let window = web_sys::window().expect("should have a window in this context");
+    let document = window.document().expect("window should have a document");
+
+    // Below are some more advanced usages of the `Closure` type for closures
+    // that need to live beyond our function call.
+
+    // Add a clock with #current-time that updates every second.
+    let body = document.body().expect("document should have a body");
+    body.set_inner_html(
+        r#"<canvas id="canvas" height="150" width="150"></canvas>"#,
+    );
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document
+        .create_element("canvas").unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+    document.body().unwrap().append_child(&canvas).unwrap();
+    canvas.set_width(640);
+    canvas.set_height(480);
+    canvas.style().set_property("border", "solid").unwrap();
+    let context = canvas
+        .get_context("2d").unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>().unwrap();
+    let context = Rc::new(context);
+    let pressed = Rc::new(Cell::new(false));
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
+            context.begin_path();
+            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            pressed.set(true);
+        });
+        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+    }
+    {
+        let context = context.clone();
+        let pressed = pressed.clone();
+        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
+            if pressed.get() {
+                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+                context.stroke();
+                context.begin_path();
+                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
+            }
+        });
+        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+    }
+    {
+        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
+            pressed.set(false);
+            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
+            context.stroke();
+        });
+        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref()).unwrap();
+        closure.forget();
+    }
+}
